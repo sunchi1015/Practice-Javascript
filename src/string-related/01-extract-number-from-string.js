e3b5208606bb437fcbf982a0c7edca1ffc130577
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2020-08-09 17:55:18 +0530

    some javascript methods

--- src/string-related/01-extract-number-from-string.js
+++ src/string-related/01-extract-number-from-string.js
@@ -5,3 +5,7 @@ const res = str.split("(");
 const trm = res[1].trim().charAt(0);
 
 console.log(trm);
+
+const row = "₹. 100000";
+const ex = row.split(".")[1].trim();
+console.log("EX:--->", Number(ex));
