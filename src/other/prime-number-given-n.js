9b59f81840526a0da333de06511d99011b849bbb
sunchi1015 <67795465+sunchi1015@users.noreply.github.com>
2021-03-07 17:54:24 +0530

    prime number for 2 to N

--- src/other/prime-number-given-n.js
+++ src/other/prime-number-given-n.js
@@ -1,40 +1,51 @@
 console.log("Prime Number for given N");
 
-// function primeNumberGen(n) {
-//   for (let i = 2; i < n; i++) {
-//    console.log('prime', )
-//   }
-// }
-
-// primeNumberGen(2);
-
-// new solutions
-function primeGen(n) {
-  let arr = [];
-  let j = 2;
-  while (j <= Number.POSITIVE_INFINITY) {
-    const res = isPrimes(j);
-    if (res) {
-      arr.push(j);
-    }
-    if (arr.length === n) {
-      break;
-    }
-    j++;
+function primeNumberGen(n) {
+  for (let i = 2; i < n; i++) {
+    console.log("prime");
+    const res = isPrime(i);
+    console.log("Prime 2 to N", res);
   }
-  return arr;
 }
 
-function isPrimes(n) {
-  var primes = true; // let prime is true
+function isPrime(n) {
   for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      primes = false; // return prime is false
-      break; // break the loop
-    } // end if inner
-  } // end inner loop
-  return primes; // return the prime true or false
-} // end the function
+    if (n % 1 === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
-const res = primeGen(3);
-console.log("prime for n", res);
+primeNumberGen(2);
+
+// new solutions
+// function primeGen(n) {
+//   let arr = [];
+//   let j = 2;
+//   while (j <= Number.POSITIVE_INFINITY) {
+//     const res = isPrimes(j);
+//     if (res) {
+//       arr.push(j);
+//     }
+//     if (arr.length === n) {
+//       break;
+//     }
+//     j++;
+//   }
+//   return arr;
+// }
+
+// function isPrimes(n) {
+//   var primes = true; // let prime is true
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       primes = false; // return prime is false
+//       break; // break the loop
+//     } // end if inner
+//   } // end inner loop
+//   return primes; // return the prime true or false
+// } // end the function
+
+// const res = primeGen(3);
+// console.log("prime for n", res);
